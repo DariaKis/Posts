72a1b0fd5280151e2bc358a81d00f51fdf13502a
Даш Киселёва <darida@MacBook-Air-Das.local>
2021-11-09 15:03:21 +0300

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,39 @@
-import React, {useMemo, useRef, useState} from 'react';
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
+import React from 'react';
 import '../src/styles/App.css'
-import PostItem from "./components/PostItem";
-import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
-import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./components/UI/MyModal/MyModal";
+import {BrowserRouter, Route, Link, Switch, Redirect} from "react-router-dom";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
+import Navbar from "./components/UI/Navbar/Navbar";
+import Error from "./pages/Error";
+import AppRouter from "./components/AppRouter";
+import {AuthContext} from'./context'
+import {useState} from "react";
+import {useEffect} from "react";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'df', body: 'rrrrrr'},
-        {id: 2, title: 'hthyht', body: 'frgrgr'},
-        {id: 3, title: '1111', body: 'oiiui777u'},
-        {id: 4, title: 'wsfewfeew', body: '3333'},
-    ])
 
-    const [filter, setFilter] = useState({sort: '', query: ''})
+    const [isAuth, setIsAuth]=useState(false);
+    const [isLoading, setLoading]=useState(true);
 
-    const sortedPosts = useMemo(() => {
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+    useEffect(()=>{
+        if (localStorage.getItem('auth')){
+            setIsAuth(true)
         }
-        return (posts)
-    }, [filter.sort, posts])
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-    }, [filter.query, sortedPosts])
-
-
-    const createPost = (newPost) => {
-        setPosts([...posts, newPost])
-    }
-
-    const removePost = (post) => {
-        setPosts(posts.filter(p => p.id !== post.id))
-    }
-
-
+        setLoading(false)
+    }, []);
     return (
-        <div className="App">
-            <MyModal>
-                <PostForm create={createPost}/>
-            </MyModal>
-            <hr style={{margin: '15px 0'}}/>
-            <PostFilter
-                filter={filter}
-                setFilter={setFilter
-                }/>
-            <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"Список постов 1"}/>
-        </div>
-    );
+        <AuthContext.Provider value={{
+            isAuth,
+            setIsAuth,
+            isLoading
+        }}>
+            <BrowserRouter>
+                <Navbar/>
+                <AppRouter/>
+            </BrowserRouter>
+        </AuthContext.Provider>
+
+    )
 }
 
 export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+
+        <h1 className="about_inf">
+            Основная информация
+        </h1>
+
+    );
+};
+
+export default About;
